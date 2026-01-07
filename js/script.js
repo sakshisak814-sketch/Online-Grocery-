3eea9e4e7f801ee1c5530fe93e245ef12a1d3b4c
sakshisak814-sketch <sakshisak814@gamil.com>
2026-01-07 16:18:38 +0530

    created cart

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ document.querySelector('#search-btn').onclick = () =>
 {
     searchForm.classList.toggle('active');
 }
+
+ 
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>
+{
+    shoppingCart.classList.toggle('active');
+}
